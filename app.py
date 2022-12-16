c497de5222865da1d081ccf77bbe4f462787f200
psrinathmanoj <44935754+psrinathmanoj@users.noreply.github.com>
2022-12-16 15:37:21 +0530

    Update app.py
    dashboard

--- app.py
+++ app.py
@@ -1,26 +1,48 @@
 import streamlit as st
-import numpy as np
 import pandas as pd
-
 import gspread
 from gspread_dataframe import get_as_dataframe
+import datetime
+
+st.set_page_config(
+    page_title="Real-Time TVS Dunzo Dashboard",
+    page_icon="✅",
+    # layout="wide",
+)
 
 # Use your Google Account credentials to authenticate with the Google Sheets API
 gc = gspread.service_account(filename='tvs-dunzo-1223551bd505.json')
 # Open the desired spreadsheet using its URL or name
-spreadsheet = gc.open_by_url('https://docs.google.com/spreadsheets/d/1LioVLgdMt1tezAM5eqvDtjc983X3cFGxOXQVLaRmy1Q/edit')
+spreadsheet = gc.open_by_url('https://docs.google.com/spreadsheets/d/1MLkJPNFsPLTcy33G971c06a_24GARp-yMvUp-zO5cxE/edit')
 
 # Select the desired worksheet from the spreadsheet
-worksheet = spreadsheet.worksheet('Sheet1')
-
+SOH = spreadsheet.worksheet('SOH')
 # Use the `get_as_dataframe` function to convert the data in the worksheet to a pandas DataFrame
-df = get_as_dataframe(worksheet)
-st.title("Hello World")
-#soh = st.file_uploader("Unload a CSV file of SOH")
-# df = pd.read_csv(soh)
-df = df[["SKU","SKU Desc","Bin","Lot No","Available Qty","Committed Qty","Lottable03","Zone","Inv Bucket"]]
+df = get_as_dataframe(SOH)
+st.title("TVS Dunzo Dashboard ")
+df = df[["SKU","SKU Desc","Bin","Lot No","Available Qty", "Commited Qty","Shelf Life % Rem","Zone"]]
+skulist = set(df["SKU"])
+totalavl = df["Available Qty"].sum()
+totalcom = df["Commited Qty"].sum()
 
-gsoh = df[df["Inv Bucket"] == "Good"]
 
-df2 = gsoh.groupby("SKU Desc").sum()
-st.dataframe(df2)
+today = datetime.datetime.today()
+# Subtract one day from the current date
+yesterday = today - datetime.timedelta(days=1)
+yesterday = yesterday.strftime('%d/%m/%Y')
+dailySOH = spreadsheet.worksheet('Daily SOH')
+dailySOH = get_as_dataframe(dailySOH)
+dailySOH = dailySOH[["Date","No Of SKU's","Qty"]]
+yessoh = dailySOH[dailySOH["Date"] == yesterday]
+yessku = yessoh["No Of SKU's"]
+yessku = int(yessku.iloc[0])
+yesto = int(len(skulist)) - yessku
+yesav = yessoh["Qty"]
+yesav = int(totalavl) - int(yesav.iloc[0])
+
+col1, col2, col3 = st.columns(3)
+col1.metric("No. of SKU's", len(skulist), yesto)
+col2.metric("Total Avl", totalavl, yesav)
+col3.metric("Total Commited Qty",totalcom)
+df2 = df[["Zone","Available Qty","Commited Qty"]].groupby("Zone").sum()
+st.bar_chart(df2)
